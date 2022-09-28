5f84f0265e7c237f72866a32ae2370261b2970fa
TingYun-Ke <mp6wu6dk33@gmail.com>
2022-09-28 10:46:10 +0800

    InitialCommit

--- sln_finalDemo/prj_finalDemo/Controllers/ApplicationController.cs
+++ sln_finalDemo/prj_finalDemo/Controllers/ApplicationController.cs
@@ -62,8 +62,6 @@ namespace prj_finalDemo.Controllers
             }
         }
         // POST: Application/Create
-        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
-        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "identity,county,schoolName,suject")] TableApplications1091704 tableApplications1091704)
@@ -93,8 +91,6 @@ namespace prj_finalDemo.Controllers
             return View(tableApplications1091704);
         }
         // POST: Application/Edit/5
-        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
-        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "identity,county,schoolName,suject")] TableApplications1091704 tableApplications1091704)
--- sln_finalDemo/prj_finalDemo/Controllers/ExamineeController.cs
+++ sln_finalDemo/prj_finalDemo/Controllers/ExamineeController.cs
@@ -44,8 +44,6 @@ namespace prj_finalDemo.Controllers
             return View();
         }
         // POST: Examinee/Create
-        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
-        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
